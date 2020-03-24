34cd31ce9b41dfb97f32f217303bc2e4b566d0a0
Jakimovska <tamarajakimovska7@gmail.com>
2020-03-24 14:12:08 +0100

    point class finished

--- Point.java
+++ Point.java
@@ -6,8 +6,29 @@ class Point {
 	//TODO add new variable
 
 	//TODO constructor
-
+        this.id = id
+        this.x = x
+        this.y = y
+        this.s = s
 	//TODO setters and getters
+        String *getId(){
+        return id }
+        double getX() {
+        return x; }
+        double getY() {
+        return y; }
+        String getS() {
+        return s; }
+    
+        void setID(String id){
+        this.id =id; }
+        void setX(double x){
+        this.x=x; }
+        void setY(double y){
+        this.y=y; }
+        void setS(String s) {
+        this.s=s; }
+       
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
